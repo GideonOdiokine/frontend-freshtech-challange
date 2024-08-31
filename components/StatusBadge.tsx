b52eb8301a66de64f1f77f1446ae5f7bba9ee685
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-08-31 06:58:15 +0100

    feat: added status code

--- components/StatusBadge.tsx
+++ components/StatusBadge.tsx
@@ -1,22 +1,15 @@
 import clsx from "clsx";
 import Image from "next/image";
 
-import { StatusIcon } from "@/constants";
 
 export const StatusBadge = ({ status }: { status: Status }) => {
   return (
-    <div
-      className={clsx("status-badge", {
-        "bg-green-600": status === "scheduled",
-        "bg-blue-600": status === "pending",
-        "bg-red-600": status === "cancelled",
-      })}
-    >
+    <div className={clsx("status-badge", {})}>
       <p
         className={clsx("text-12-semibold capitalize", {
-          "text-green-500": status === "scheduled",
-          "text-blue-500": status === "pending",
-          "text-red-500": status === "cancelled",
+          "text-[#2DAE32]": status === "successful",
+          "text-[#EE5D50]": status === "failed",
+          "text-[#FFB547]": status === "initiated",
         })}
       >
         {status}
