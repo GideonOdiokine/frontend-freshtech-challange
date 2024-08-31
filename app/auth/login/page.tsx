f65917bf544db3f36361dffa60a62a9cf05f3f4d
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-08-31 07:21:07 +0100

    update gobackbtn

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -8,9 +8,9 @@ const Login = () => {
     <section className="flex-center size-full relative bg-[#EFF3FB]">
       <div className="flex justify-between absolute left-0 h-fit py-6 px-8">
         <GoBackButton label="Home" />
-        <Button className="bg-[#4169E1] fixed right-10">Sign Up</Button>
+        <Button className="bg-[#4169E1] text-white fixed  right-10">Sign Up</Button>
       </div>
-      <div className="container md:pt-8 lg:!px[223px] md:!px-[150px]">
+      <div className="container pt-12 md:pt-8 lg:!px[223px] md:!px-[150px]">
         <LoginForm />
       </div>
     </section>
